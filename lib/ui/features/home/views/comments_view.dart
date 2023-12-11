5e52fcc51ced53dbfbe4697f43a971f94290fde2
JER3MIAH <jeremiahbrownie@gmail.com>
2023-12-11 10:41:27 +0100

    Add custom widgets and utils

---
+++ lib/ui/features/home/views/comments_view.dart
@@ -0,0 +1,143 @@
+// ignore_for_file: use_build_context_synchronously, prefer_typing_uninitialized_variables
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/common/utils/colors.dart';
+import 'package:flutter_instagram_clone/common/utils/utils.dart';
+import 'package:flutter_instagram_clone/common/widgets/comment_card.dart';
+import 'package:flutter_instagram_clone/data/models/user.dart';
+import 'package:flutter_instagram_clone/logic/providers/user_provider.dart';
+import 'package:flutter_instagram_clone/logic/services/firestore_service.dart';
+import 'package:provider/provider.dart';
+
+class CommentsScreen extends StatefulWidget {
+  final postId;
+  const CommentsScreen({super.key, required this.postId});
+
+  @override
+  State<CommentsScreen> createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends State<CommentsScreen> {
+  final TextEditingController commentEditingController =
+      TextEditingController();
+
+  void postComment(String uid, String name, String profilePic) async {
+    try {
+      String res = await FirestoreService().postComment(
+        widget.postId,
+        commentEditingController.text,
+        uid,
+        name,
+        profilePic,
+      );
+
+      if (res != 'success') {
+        if (context.mounted) showSnackBar(context, res);
+      }
+      setState(() {
+        commentEditingController.text = "";
+      });
+    } catch (err) {
+      showSnackBar(
+        context,
+        err.toString(),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final User user = Provider.of<UserProvider>(context).getUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: const Text(
+          'Comments',
+        ),
+        centerTitle: false,
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('posts')
+            .doc(widget.postId)
+            .collection('comments')
+            .snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (ctx, index) => CommentCard(
+              snap: snapshot.data!.docs[index],
+            ),
+          );
+        },
+      ),
+      // text input
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          padding: const EdgeInsets.only(left: 16, right: 8),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.photoUrl),
+                radius: 18,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 8),
+                  child: TextField(
+                    controller: commentEditingController,
+                    decoration: InputDecoration(
+                      hintText: 'Comment as ${user.username}',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () => postComment(
+                  user.uid,
+                  user.username,
+                  user.photoUrl,
+                ),
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
